872cc4257aec26226656e7a449fc122e9b36503a
riccardoscheda <riccardo.scheda@studio.unibo.it>
2020-02-19 11:48:43 +0100

    random matrix

--- net.py
+++ net.py
@@ -17,18 +17,25 @@ dt = 0.1
 ###################################
 fig = plt.figure()
 
-matrix = np.ones((10,10))
+matrix =  np.random.randint(2, size=(10,10))
 
 def init():
 
+    
     im = plt.imshow(matrix)
+    
     return im,
 
 
 def evo(frames):
-
+    plt.clf()
+    
+    for i in range(10):
+        for j in range(10):    
+            matrix[i,j] = matrix[j,i]*matrix[i,j]
     im = plt.imshow(matrix)
+    print(matrix)
     return im
 
-ani = FuncAnimation(fig, evo, frames = np.arange(0,100), interval = 50,init_func = init, blit = True)
+ani = FuncAnimation(fig, evo, frames = np.arange(0,100), interval = 200,init_func = init, blit = False)
 

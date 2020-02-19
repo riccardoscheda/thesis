a83d12661b9eec91cc7fc23d1fd1862f3c7076a2
riccardoscheda <riccardo.scheda@studio.unibo.it>
2020-02-19 16:02:25 +0100

    problems with multiplication

--- net.py
+++ net.py
@@ -11,15 +11,22 @@ import pandas as pd
 #iterations
 frames = 100
 #particles
-n = 10
+n = 4
 #time step
 dt = 0.1
 ###################################
 fig = plt.figure()
 
-matrix =  np.random.randint(2, size=(10,10))
+#matrix =  np.random.randint(2, size=(n,n))
+matrix = np.array([[0,1,0,0],
+          [0,0,1,1],
+          [1,0,0,0],
+          [0,0,1,0]])
+nodes = np.zeros((n,1))
+up = np.zeros((n,1))
+#initial state
+nodes[0] = 1
 
-nodes = np.ones((10,10))
 def init():
 
     
@@ -28,14 +35,24 @@ def init():
     return im,
 
 
+
 def evo(frames):
     plt.clf()
-    for i in range(n):
-        for j in range(n):
-            nodes[i][j] = matrix[i][j] + nodes[i][j]
     
-    im = plt.imshow(nodes)
-    #print(nodes)
+
+    for i in range(n):
+        somma = 0
+        for j in range(n):
+            somma  = somma + matrix[i][j] * nodes[i]
+        up[i]= somma
+    
+    
+    
+    im = plt.imshow(up)
+    
+    for i in range(n):
+        nodes[i] = up[i]
+    print(nodes)
     return im
 
 ani = FuncAnimation(fig, evo, frames = np.arange(0,100), interval = 200,init_func = init, blit = False)

39dd788fc57e87d91722113f6436579700a7b5ae
riccardoscheda <riccardo.scheda@studio.unibo.it>
2020-02-20 10:37:20 +0100

    ok, now it works, with some restrictions to adjancency matrix, and it goes in senso orario invece che antiorario (?)

--- net.py
+++ net.py
@@ -19,7 +19,7 @@ fig = plt.figure()
 
 #matrix =  np.random.randint(2, size=(n,n))
 matrix = np.array([[0,1,0,0],
-          [0,0,1,1],
+          [0,0,1,0],
           [1,0,0,0],
           [0,0,1,0]])
 nodes = np.zeros((n,1))
@@ -38,21 +38,23 @@ def init():
 
 def evo(frames):
     plt.clf()
-    
+    up = np.zeros((n,1))
 
-    for i in range(n):
-        somma = 0
-        for j in range(n):
-            somma  = somma + matrix[i][j] * nodes[i]
-        up[i]= somma
+    # for i in range(n):
+    #     somma = 0
+    #     for j in range(n):
+    #         somma  = somma + matrix[i][j] * nodes[i]
+    #    # up[i]= somma
     
     
-    
+    up = matrix.dot(nodes)
+
     im = plt.imshow(up)
     
     for i in range(n):
         nodes[i] = up[i]
-    print(nodes)
+    print(nodes.T)
+    
     return im
 
 ani = FuncAnimation(fig, evo, frames = np.arange(0,100), interval = 200,init_func = init, blit = False)

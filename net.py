de4963493ba9ee088d3141f3a542c8b09c725dbc
riccardoscheda <riccardo.scheda@studio.unibo.it>
2020-02-25 12:43:05 +0100

    yessssss, now we have to work to the adjancency matrix

--- net.py
+++ net.py
@@ -11,17 +11,20 @@ import networkx as nx
 #iterations
 frames = 100
 #particles
-n = 4
+n = 100
 #time step
 dt = 0.1
 ###################################
-fig = plt.figure()
+fig, ax = plt.subplots(2,1)
 
 #matrix =  np.random.randint(2, size=(n,n))
-matrix = np.array([[0,1,0,0],
-          [0,0,1,0],
-          [1,0,0,0],
-          [0,0,1,0]])
+# matrix = np.array([[0,1,0,0,0],
+#           [0,0,1,0,0],
+#           [1,0,0,0,0],
+#           [0,1,0,0,0],
+#           [0,0,1,0,0]])
+
+
 nodes = np.zeros((n,1))
 up = np.zeros((n,1))
 #initial state
@@ -29,20 +32,35 @@ nodes[0] = 1
 
 g = nx.Graph()
 
-def init():
-
-    
-    im = plt.imshow(nodes)
-    
-    return im,
-
 
 def decimal(u):
     b = '\n'.join(''.join('%d' %x for x in y) for y in u)
     return int(b,2)
     
+def random_adjancency_matrix(n):
+    matrix = np.zeros((n,n))
+    for i in range(n):
+        matrix[i][np.random.randint(n)] = 1
+
+    return matrix
+
+matrix = random_adjancency_matrix(n).T
+print(matrix)
+
+def init():
+
+    
+    net = ax[0].imshow(nodes.T)
+    ax[1] = nx.draw(g)
+
+
+    return ax[0],
+
+
+
 def evo(frames):
-    plt.clf()
+    plt.ion()
+    plt.cla()
     up = np.zeros((n,1))
 
     # for i in range(n):
@@ -54,7 +72,7 @@ def evo(frames):
     
     up = matrix.dot(nodes)
 
-    #im = plt.imshow(up)
+    #ax[0] = plt.imshow(up)
     previous_state = decimal(nodes.T)
     
     for i in range(n):
@@ -62,9 +80,12 @@ def evo(frames):
         
     state = decimal(nodes.T)
     g.add_edge(previous_state,state)
-    #print(g.nodes())
-    nx.draw(g)
-    return im
+
+    ax[0].imshow(nodes.T)
+    ax[1] = nx.draw_circular(g)
+    
+    
+    return  ax[0], ax[1] 
 
 ani = FuncAnimation(fig, evo, frames = np.arange(0,100), interval = 200,init_func = init, blit = False)
 

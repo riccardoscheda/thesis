ea4d57346069cd29df1a0a4aee6648b46d0bfe9d
riccardoscheda <riccardo.scheda@studio.unibo.it>
2020-04-01 10:38:41 +0200

    started new framework for master eq, need to fix obviously

--- FP.py
+++ FP.py
@@ -1,26 +1,178 @@
+
 import numpy as np
 import pylab as plt
+from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation # animation plot
+import pandas as pd
+
+import integration as inte
 
 
 #number of genes
-n = 50
+n = 100
 #transition matrix
 W = np.random.uniform(-1,1,size = (n,n))
 #mean lifetime of ecited states
 gamma = np.random.uniform(0,1,size = (n))
 
+
 #probability tha the node is in the state 1
 p = np.zeros(n)
 p[25] = 1
 
-plt.plot(p)
+
 #laplacian matrix
 L = W - gamma*np.identity(n)
 
 
 for i in range(10):
     p = sum(L)*p - gamma*p
-    plt.plot(np.linspace(0,n,num = n),p)
+    #plt.plot(np.linspace(0,n,num = n),p)
     
+    
+    
+############PARAMETERS######################################
+gamma = 1.
+eps = 0.6
+KT = eps**2/(2*gamma)
+m = 1
+################## HARMONIC OSCILLATOR ######################
+#eps = 0.
+#gamma = .0
+############################################################
+
+
+#iterations
+frames = 100
+#particles
+N = n
+#time step
+dt = 0.1
+
+##############################################################
+qx, px = np.ones(N), -np.ones(N)*10
+
+rhox, binx = np.histogram(qx,density = 1,bins=50)
+rhop, binp = np.histogram(px,density = 1,bins=50)
+
+varx = rhox.var()
+varp = rhop.var()
+
+var = varx + varp
+########################################################
+fig, ax = plt.subplots(2,2)
+
+particle, = ax[0,0].step([],[], label = "rho_p")
+trajectory, = ax[0,0].step([],[], label = "fit")
+maxwelldist, = ax[1,0].step([],[],label = 'distribution')
+maxwellfit, = ax[1,0].plot([],[],label = "fit")
+ax[0,0].legend()
+ax[1,0].legend()
+phasespace, = ax[0,1].step([],[])
+entr, = ax[1,1].plot([],[], label = "S")
+shan, = ax[1,1].plot([],[], linestyle = "--", label = "S_infty")
+ax[1,1].legend()
+
+
+tx = []
+tp = []
+entropy = []
+shannon = []
+
+
+
+def init():
+
+  ax[0,0].set_xlim(-2,2)
+  ax[0,0].set_ylim(0,0.05)
+  ax[0,0].set_title("Distribution of momenta")
+  ax[0,0].set_xlabel("p")
+  ax[0,0].set_ylabel("rho_p")
+
+  ax[0,1].set_xlim(-5,5)
+  ax[0,1].set_ylim(0,0.1)
+  ax[0,1].set_xlabel("x")
+  ax[0,1].set_ylabel("rho_x")
+  ax[0,1].set_title("Distribution of positions")
+
+  ax[1,0].set_xlim(-0.1, 5)
+  ax[1,0].set_ylim(-0.001, 0.1)
+  ax[1,0].set_xlabel("v")
+  ax[1,0].set_ylabel("rho(v)")
+  ax[1,0].set_title("velocity Distribution")
+
+  ax[1,1].set_xlim(0, 300)
+  ax[1,1].set_ylim(-5, 5)
+  ax[1,1].set_xlabel("time")
+  ax[1,1].set_ylabel("entropy")
+  ax[1,1].set_title("Entropy")
+
+  return particle,
+
+def evo(frames):
+    for i in range(N):
+      s = np.sum(W[i])
+      qx[i], px[i] = inte.simplettic(qx[i], px[i],dt,eps  ,gamma,s,i)
+
+    vel = np.sqrt(px**2)/m
+
+    bins = np.arange(np.floor(qx.min()),np.ceil(qx.max()))
+    n = 100
+
+    rhox, binx = np.histogram(qx,density = 1, bins = 100)
+    rhop, binp = np.histogram(px,density = 1,bins = 50)
+
+
+    hist = np.histogram(vel,density = 1, bins=50)
+
+
+
+    #tx.append(qx[0])
+    #tp.append(px[0]/m)
+
+    rhox += 1e-35
+    rhop += 1e-35
+    #entropy.append(-np.sum(rhox*rhop*np.log(rhox*rhop)*np.diff(binx)*np.diff(binp)))
+
+    x = np.linspace(-10, 10, num = 1000)
+    maxwellpdf = np.sqrt(m/(2*np.pi*KT))*np.exp(-x**2*m/(2*KT))/50
+    trajectory.set_data(x, maxwellpdf)
+    particle.set_data(binp[1:], rhop/50)
+    phasespace.set_data(binx[1:],rhox/50)
+
+    ####################################################
+    space = np.array(qx)
+    space = np.vstack((space,px))
+
+    hist_space, bins = np.histogramdd(np.array(space.T),bins =50,density = 1)#,range = (interval,interval,interval,interval,interval,interval))
+    hist_space += 1e-35
+
+    entropy.append(-np.sum(hist_space*np.log(hist_space)*np.diff(bins[0])*np.diff(bins[1])))
+
+
+
+    ###########################################################
+    #fit
+    x = np.linspace(0, 10, num = 1000)
+    maxwellpdf = 2*np.sqrt(m/(2*np.pi*KT))*np.exp(-x**2*m/(2*KT))/50
+
+    maxwelldist.set_data(hist[1][1:],hist[0]/50)
+
+    maxwellfit.set_data(x, maxwellpdf)
+    entr.set_data(np.arange(0,len(entropy)),entropy)
+
+
+
+    x = np.linspace(0, 1000, num = 1000)
+    y = np.ones(1000)*(np.log(KT) + 0.5*np.log(var))
+    shan.set_data(x,y)
+
+    return particle, trajectory, phasespace, maxwelldist, maxwellfit, entr, shan
+
+
+ani = FuncAnimation(fig, evo, frames = np.arange(0,100), interval = 50,init_func = init, blit = True)
+#plt.tight_layout()
+#ani.save('Doublewell.gif', dpi=140, writer='imagemagick')
+
   
 
---
+++ integration.py
@@ -0,0 +1,104 @@
+########## file for the functions for the integration of the system ###########
+
+import numpy as np
+
+
+def mod(v):
+    """
+    Computes the module of a vector
+    """
+    return np.sqrt(sum(v*v,axis=0))
+
+def phi(q,p,W,i):
+    """
+    The vectorial field which generates the evolution in the pase space
+    Parameters:
+    ---------------------------------
+    q : the generalized coordinate, float
+    p : the generalized momenta, float
+    omega : frequency, float
+
+    Return the derivative of the potential with respect to q.
+    """
+
+    return p , 0
+
+
+def entropy(rho,bins):
+    """
+    Computes the entropy of the system.
+    Parameters:
+    ---------------------------------------
+    rho: the probability distribution of the system
+    bins: the bins of the distribution
+
+    Returns the entropy as a float
+    """
+
+    return np.sum(rho*np.log(rho)*np.diff(bins))
+
+
+def Shannon_entropy(N,qx,qy,qz,px,py,pz):
+
+
+  space = np.array(qx)
+
+  space = np.vstack((space,qy))
+  space = np.vstack((space,qz))
+  space = np.vstack((space,px))
+  space = np.vstack((space,py))
+  space = np.vstack((space,pz))
+
+
+  
+  hist_space, bins = np.histogramdd(np.array(space.T), density = 1,bins = (2,2,2,2,2,2))#,range = (interval,interval,interval,interval,interval,interval))
+
+  hist_space += 1e-35
+
+
+  return hist_space, bins
+
+#####################################################################
+#####################################################################
+
+def euler(q,p,dt ,eps,gamma):
+    """
+    Euler method to obtain the evolution of the system
+    Parameters:
+    ------------------------------------------
+    q : the generalized coordinate, float
+    dt : the evolution time step, float
+    eps : constant which scale the intensity of the white noise csi
+    gamma : dumping constant (?)
+
+    Returns the evolution of the coordinates q and p
+
+    """
+    # white noise
+    csi = np.random.normal(0, 1)
+    #evolution of the coordinates q and p
+    evoq = q + phi(q,p)[0]*dt
+    evop = p -gamma*p*dt + phi(q,p)[1]*dt + eps*np.sqrt(dt)*csi
+    return evoq, evop
+
+
+def simplettic(q,p,dt,eps,gamma,W,i):
+    """
+    Simplettic integration method to obtain the evolution of the system
+    Parameters:
+    ------------------------------------------
+    q : the generalized coordinate, float
+    dt : the evolution time step, float
+    eps : constant which scale the intensity of the white noise csi
+    gamma : damping constant (?)
+
+    Returns the evolution of the coordinates q and p
+
+    """
+    # white noise
+    csi = np.random.normal(0, 1.)
+    eps = 0 
+    #evolution of the coordinates q and p
+    evoq = q + phi(q,p + dt*phi(q, p,W,i)[1],W,i)[0]*dt
+    evop = p -gamma*p*dt + phi(q,p,W,i)[1]*dt + eps*np.sqrt(dt)*csi
+    return evoq, evop

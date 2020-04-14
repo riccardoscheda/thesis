94b17bc2817ca8c8d155418d1e938165d96612a6
riccardoscheda <riccardo.scheda@studio.unibo.it>
2020-04-14 11:46:41 +0200

    added laplacian matrices

--- randomatrix.py
+++ randomatrix.py
@@ -11,11 +11,21 @@ import numpy as np
 import pylab as plt 
 
 #number of genes
-n = 5000
+n = 1000
 #transition matrix
 W = np.random.normal(0,1,size = (n,n))
 
+#mean lifetime of ecited states
+gamma = np.ones(n)*2.
+gammahat = sum(W.T)
+gammahat
+deltagamma = gamma - gammahat
+deltagamma
+#deltagamma = np.ones(n)*2
+#laplacian matrix
+L = W - gammahat*np.identity(n)
+
 Ws = (W + W.T) / 2
-eigenvalues , _ = np.linalg.eig(W)
+eigenvalues , _ = np.linalg.eig(L)
 
 plt.hist(eigenvalues,bins = 30,density= True)

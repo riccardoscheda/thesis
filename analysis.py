4a3a17fa73389e068baf2cd5f6eb5eba021c4a59
riccardoscheda <riccardo.scheda@studio.unibo.it>
2020-06-13 16:58:53 +0200

    now i have the plot of the number of active nodes depending on the different initial conditions.

--- analysis.py
+++ analysis.py
@@ -1,8 +1,50 @@
 import pylab as plt
+import numpy as np
 import networkx as nx
 import  random_network  as rn
 
-g = rn.Random_Network(10, 2)
+N = 20
+K = 1
+
+number_of_active_nodes = []
+g = rn.Random_Network(N, K)
 A = g.adj_matrix
 
-plt.imshow(A)
+
+#plt.imshow(A)
+
+
+def evolution(time):
+    
+    for t in range(time):
+        up = np.zeros((N,1))
+        for i in range(N):
+            somma = 0 
+            for j in range(N):
+                somma = somma + g.adj_matrix[i][j]*g.nodes[j]
+            up[i] = somma
+            
+        for i in range(N):
+            if up[i] != 0:
+                g.nodes[i] = 1
+            else:
+                g.nodes[i] = 0 
+
+def shuffle():
+    for i in range(N):
+        for j in range(N):
+            g.nodes[j] = 0
+        g.nodes[i] = 1
+        evolution(20)
+        number_of_active_nodes.append(np.sum(g.nodes))
+
+shuffle()
+print(number_of_active_nodes)
+
+graph = nx.from_numpy_matrix(A.T,create_using = nx.DiGraph)
+pos = nx.layout.spring_layout(graph)
+plt.figure() 
+plt.plot(number_of_active_nodes, label = "number of active nodes")
+plt.legend()
+#nx.draw_networkx(graph, pos = pos, with_labels= True)
+
--- random_network.py
+++ random_network.py
@@ -4,10 +4,10 @@ class Random_Network:
     def __init__(self, N, K):
         self.n = N
         self.k = K
+        self.nodes = np.zeros((self.n,1))
 
         self.adj_matrix = np.zeros((self.n,self.n))
         for i in range(self.n):
             for j in range(self.k):
                 self.adj_matrix[i][np.random.randint(self.n)] = 1
         
-    

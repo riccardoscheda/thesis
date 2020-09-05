0d08ac98b442dab59ed600fa3da84fbe9a68379a
riccardoscheda <riccardo.scheda@studio.unibo.it>
2020-09-05 17:15:19 +0200

    WRITING: finishing celldiff and grn chapters

--- makenetwork.py
+++ makenetwork.py
@@ -8,31 +8,29 @@ import network2tikz
 PATH = "tesi/"
 N = 4
 K = 1
-graph = rn.Random_Network(N, K)
 
-nodes = [str(i+1) for i in range(N)]
-graph.edges
-edges = graph.edges
+nodes = ['1','2','3','4','5','6']
+edges = [('1','2'),('2','3'),('2','4'),('4','5'),('4','6')]
 gender = ['w' for i in range(N)]
 colors = {'g': 'gray', 'w': 'white'}
 g = nx.DiGraph(edges)
 style = {}
 style["vertex_shape"] = "circle"
-style['vertex_size'] = .4
+style['vertex_size'] = .9
 style["edge_width"] = 0.8
-style["edge_label"] = ["+1","+1","+1","+1","+1"]
-style['node_label'] = nodes
+#style["edge_label"] = ["+1","+1","+1","+1","+1"]
+#style['node_label'] = nodes
 style['node_label_size'] = 6
-style['node_color'] = [colors[g] for g in gender]
+#style['node_color'] = [colors[g] for g in gender]
 #style['node_opacity'] = .5
 style['edge_curved'] = .0
 
 
 #pos = nx.layout.spring_layout(g,k=5/np.sqrt(g.order()))
 pos = nx.kamada_kawai_layout(g)
-layout = nx.layout.spring_layout(g,pos = pos,iterations = 0)
+layout = nx.layout.spring_layout(g,pos = pos,iterations = 2)
 
-plot((g),PATH + 'prova.tex',layout= layout,**style)
+plot((g),PATH + 'cells.tex',layout= layout,**style)
 
 import os
 
--- model.py
+++ model.py
@@ -1,14 +1,11 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from random import choice
 from functools import reduce
-
 import matplotlib.animation as animation # animation plot
 import pandas as pd
 import networkx as nx
-
 import  random_network  as rn
 
 fig, ax = plt.subplots(1,1)
@@ -103,15 +100,108 @@ print(rn.outgoing_links(Net, N))
 ani = FuncAnimation(fig, evo, frames = np.arange(0,500), interval = 200,init_func = init, blit = False)
 #ani.save('network.gif',dpi = 100,writer = "imagemagick")
 
-#print("outgoing links: " + str(sum(tot)))
-#print("cycles: " + str(cycles))
-#driver_node = list(reduce(lambda x,y: set(x)&set(y),cycles))
-# final = []
-# z = list(reduce(lambda x,y: x+y,cycles))
-# print("driver node: "+ str(driver_node))
-# for i in range(N):
-#     final.append(z.count(i))
-# control_node = np.argmax(final)
-# print(control_node)
 
-#np.savetxt("network.txt", tot, fmt="%d",delimiter=" ")
+#%% 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from random import choice
+from functools import reduce
+import matplotlib.animation as animation # animation plot
+import pandas as pd
+import networkx as nx
+import  random_network  as rn
+
+fig, ax = plt.subplots(1,1)
+
+#iterations
+frames = 500
+
+N = 10
+K = 2
+number_of_clusters = 2
+
+######################################
+
+#creation of the subnetworks
+gr = [rn.Random_Network(N,K)]
+gr.append(rn.Random_Network(1, 1))
+gr[1].adj_matrix
+single_cluster_control_nodes = [rn.outgoing_links(gr[i],N) for i in range(number_of_clusters)]
+control_nodes = [single_cluster_control_nodes[i] + i*N for i in range(number_of_clusters)]
+tot = rn.create_clusters(gr, control_nodes, N,number_of_clusters)
+negedges = list(zip(list(np.where(tot.T<0)[0]),list(np.where(tot.T<0)[1])))
+
+Net = rn.Network(tot)
+graph = nx.from_numpy_matrix(tot.T, create_using=nx.DiGraph)
+npos = nx.spring_layout(graph)
+cycles = nx.cycle_basis(graph.to_undirected())
+
+################## ONLY FOR VISUALIZATION #######################
+abs_tot = abs(tot)
+graph1 = nx.from_numpy_matrix(abs_tot.T, create_using=nx.DiGraph)
+npos = nx.kamada_kawai_layout(graph1)
+#################################################################
+
+
+
+
+def init():
+    ax = nx.draw(graph,pos = npos)
+    active_nodes = []
+    non_active_nodes = []
+    for i in range(number_of_clusters):
+        Net.nodes[control_nodes[i]] = 1
+    
+    for i in range(N):
+        Net.nodes[i] = 1
+        
+        
+    for i in range(len(Net.nodes)):
+        if Net.nodes[i] == 1 :
+            active_nodes.append(i)
+        else:
+            non_active_nodes.append(i)
+    ax = nx.draw_networkx(graph,npos, with_labels= False)
+    ax = nx.draw_networkx_nodes(graph,npos,
+                        nodelist=active_nodes,
+                        node_color='y')
+
+    return ax,
+
+
+def evo(frames):
+    plt.ion()
+    plt.cla()
+    
+    up = Net.adj_matrix.dot(Net.nodes)
+    Net.nodes = (up >0).astype(int)
+    rn.noise(Net,p=0.2)
+    active_nodes = []
+    non_active_nodes = []
+    
+    for i in range(len(Net.nodes)):
+        if Net.nodes[i] == 1 :
+            active_nodes.append(i)
+        else:
+            non_active_nodes.append(i)
+    
+    ax = nx.draw_networkx(graph,npos, with_labels= True)
+    ax = nx.draw_networkx_nodes(graph,npos,
+                        nodelist=control_nodes,
+                        node_size=800)
+    ax = nx.draw_networkx_nodes(graph,npos,
+                        nodelist=active_nodes,
+                        node_color='y')
+  
+    ax = nx.draw_networkx_edges(graph, npos,
+                       edgelist=negedges,
+                       width=3, alpha=0.4, edge_color='r')
+    plt.title("frame " +str(frames))
+    return  ax
+
+print(rn.outgoing_links(gr[0], N))
+print(rn.outgoing_links(Net, N))
+ani = FuncAnimation(fig, evo, frames = np.arange(0,500), interval = 200,init_func = init, blit = False)
+#ani.save('network.gif',dpi = 100,writer = "imagemagick")
+
